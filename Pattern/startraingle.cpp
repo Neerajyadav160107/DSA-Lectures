1898dea49040ee00ab9ce363a6634cee22b25611
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-11 23:47:41 +0530

    star triangle

--- Pattern/startraingle.cpp
+++ Pattern/startraingle.cpp
@@ -13,3 +13,9 @@ int main () {
 
 
 }
+/* First we check that i < =n then fo inside the loop 
+then checks j<=1 then the condistion runs cout<<"*" for once as in the next loop j>i 
+the loops ends then 
+again goes to the initial loop and i becomes 2 and futher goes the same ...
+this program when i =1 to n & j = 1 to i
+gives a right angles triangle as output .ˍˍˍ*/
