3d92dc3bb600348bfc5fded15e1d91ca214717e4
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-13 00:10:10 +0530

    Binary Triangle

--- Pattern/BinaryTriangle.cpp
+++ Pattern/BinaryTriangle.cpp
@@ -1,3 +1,10 @@
+//WAP to print the given patterm 
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
+
 #include <iostream>
 using namespace std;
 int main(){
@@ -6,22 +13,33 @@ int main(){
     cin>>n;
     int a=5;
     
-    for ( int i =1 ; i<=n; i++){
-        if ( i%2!=0) a=1; // row no odd
-        else a =0; // row no even 
-        for ( int j=1 ; j<=i; j++){
-            cout <<a;
-            //till this we have printed the baisc structure of a  binary triangle of 1 and 0 
-            // tp get the alternate pattern of 1 and 0 we will do 
-            // flipping 
-            if ( a==1 ) a=0;
-            else a=1;
+    // for ( int i =1 ; i<=n; i++){
+    //     if ( i%2!=0) a=1; // row no odd
+    //     else a =0; // row no even 
+    //     for ( int j=1 ; j<=i; j++){
+    //         cout <<a;
+    //         //till this we have printed the baisc structure of a  binary triangle of 1 and 0 
+    //         // tp get the alternate pattern of 1 and 0 we will do 
+    //         // flipping 
+    //         if ( a==1 ) a=0;
+    //         else a=1;
+    //     }
+    //     cout <<endl;
+    for ( int i = 1 ; i<=n; i++){
+        for (int j=1;j<=i; j++){ 
+            if ( (i+j)%2==0 ) cout << 1; // condition here : if both i and j are odd or even ---> its giving 1 , else 0 when i & j !=row and column
+                                         // when both are even or odd , their sum is divisible by 2 then it will also give ---> 1;
+            else cout <<0; cout<< "  ";
         }
-        cout <<endl;
+        cout<<endl;
             
         }
     
     }
+
+
+
+
 
 
 
