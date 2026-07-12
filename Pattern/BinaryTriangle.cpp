777c163ef1008edc0a7df8832b03f2beaa2e62a7
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-12 23:49:34 +0530

    Binary Traingle Alternate Pattern Printing

---
+++ Pattern/BinaryTriangle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"no of rows:";
+    cin>>n;
+    int a=5;
+    
+    for ( int i =1 ; i<=n; i++){
+        if ( i%2!=0) a=1; // row no odd
+        else a =0; // row no even 
+        for ( int j=1 ; j<=i; j++){
+            cout <<a;
+            //till this we have printed the baisc structure of a  binary triangle of 1 and 0 
+            // tp get the alternate pattern of 1 and 0 we will do 
+            // flipping 
+            if ( a==1 ) a=0;
+            else a=1;
+        }
+        cout <<endl;
+            
+        }
+    
+    }
+
+
+
+
