86e58adf437690018e9763bc062a866a2de2c364
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-15 15:03:08 +0530

    Star diamond using nsp and nst method

--- Pattern-2/StarDiamond.cpp
+++ Pattern-2/StarDiamond.cpp
@@ -1,24 +1,43 @@
+// WAP tp print the given pattern ( n=5):
+//    *
+//   ***
+//  *****
+// *******
+//*********
+// *******
+//  *****
+//   ***
+//    *
+
+
+// ALGO : 
+// nst : no of stars - initiliazing w 1 
+// nsp : no of spaces - n-1 ; 
 #include <iostream> 
 using namespace std;
-int main () {
+int main (){
     int n;
     cout <<" Enter No of Rows:";
     cin>>n;
     int nst = 1;
     int nsp = n-1;
 
-    for ( int i =1 ; i <=n ; i++){
+    for ( int i =1 ; i <=2*n-1 ; i++){
         // spaces
         for ( int j =1 ;j <=nsp; j++){
             cout<<" " ;
 
 
         }
-        nsp-- ;
+        if ( i<=n-1) nsp--;
+        else nsp++;
         // so that the in next iteration space will be printed n-1 no of times
         // stars
         for ( int k=1; k<= nst; k++){
             cout <<"*";
         }
-        nst+=2;
-        cout << endl;
+        if ( i<=n-1) nst +=2;
+        else nst-=2;
+        cout<< endl;
+    }
+}
