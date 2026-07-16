70322bdce6f5cce638757e7e02d096b79cdbcf14
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-16 22:10:34 +0530

    Function Calling Basics

---
+++ Functions/Functioncalling.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void greeting () {
+    cout <<"Good Morning "<<endl;
+    cout <<"Have a nice day , Suraj" << endl;
+
+}
+
+
+int main () {
+    greeting() ;
+    greeting() ;
+    greeting() ;
+
+
+}
+
+
