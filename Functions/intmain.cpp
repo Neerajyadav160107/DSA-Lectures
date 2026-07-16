be38b27162c00223e21922901a964424ab0a5a7c
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-16 22:45:28 +0530

    what is int main()

---
+++ Functions/intmain.cpp
@@ -0,0 +1,23 @@
+// what is int main () 
+// runs at the 1st place 
+// int main runs once only :
+// main is a resevr function 
+// // What is return type :
+//    ex: void - runs the iteration and returns void 
+
+
+
+#include <iostream>
+using namespace std ;
+int sum (int x, int y ){
+    return x+y;
+ 
+}
+int main (){
+    cout<< sum (40,30);
+    // sum here is an integer data type as sum is an integer data type initilized above so sum has be a int data type 
+
+}
+
+//FLOWCHART
+// 10>11>16>17>12>13
