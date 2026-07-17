02f34695febd166f6b27298cfdfd4a2c5decd91d
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-17 23:22:35 +0530

    Revisting Variables

---
+++ Pointers/revisitingvariable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+int main (){
+    int x=5;
+    cout<<&x;
+}
+// what's an address of an variable ?
+// whenver we hit run button it alloactes a random available memory address to that variable .
+// and thats basically its an address .
+// &&--> means address of .  
