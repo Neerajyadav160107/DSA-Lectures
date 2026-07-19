5bf2b94b2e7679aba10f90fa04f25849dabb4e73
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-19 22:14:44 +0530

    Syntax And Declaration

---
+++ Array/SyntaxAndDeclaration.cpp
@@ -0,0 +1,23 @@
+// Array is an Data Structure to store data
+// An array is a collection of elements of the same data type
+// stored in contiguous (continuous) memory locations.
+// Each element is accessed using its index, starting from 0.
+
+// Syntax and Declaration:
+// int x[7] --> it will make 7 integer data type to store in a location
+// Indexing In an array : starts from 0th index/element.
+// let suppose if i want to store 5 in 3rd index in an array x 
+// x[3]=5 .
+// if i want to update ; we can easily update in the next iteration .
+// an array runs till (size-1)
+
+#include <iostream>
+using namespace std ;
+int main () {
+    //Declration and Initilization
+    int arr[7] ={ 1,2,3,4,5,6,8};
+
+    cout<<arr[2];
+}
+//How to access elements in array ?
+// cout<<arr[index of array];
