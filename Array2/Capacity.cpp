1f1e31c6c574d80b3f5696582f7e360856901fb7
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-25 19:49:50 +0530

    Capacity and Size in a vector

---
+++ Array2/Capacity.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int main (){
+    vector<int> v;
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(6);
+    v.push_back(7);
+    v.push_back(10);
+    v.push_back(11);
+    v.push_back(12);
+    v.push_back(13);
+    cout << "Size is :" << v.size() << endl;
+    cout << "Capacity is:" << v.capacity() << endl;
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    cout << "Size is :" << v.size() << endl;
+    cout << "Capacity is:" << v.capacity() << endl;
+
+    return 0;
+}
---
+++ Array2/OperationOfvectors.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main (){
+    vector<int> v;
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(6);
+    v.push_back(7);
+    v.push_back(10);
+    v.push_back(11);
+    v.push_back(12);
+    v.push_back(13);
+    for (int i=0;i<v.size();i++) {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    v.pop_back();
+    for (int i=0;i<v.size();i++) {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    v.pop_back();
+    for (int i=0;i<v.size();i++) {
+        cout<<v[i]<<" ";
+    }
+
+
+}
