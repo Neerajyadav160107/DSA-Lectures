33872d1736a44442cd0d8e64118b1e6e2e3e8ab0
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-27 23:19:05 +0530

    Revrsal Array using Pointer |Without Another Array

--- Array2/ReverseArraywithpointer.cpp
+++ Array2/ReverseArraywithpointer.cpp
@@ -27,12 +27,13 @@ int main () {
     //     v[j]=temp;
     //     i++;
     //     j--;
+
+    
+    // by for loop
     for ( int i=0,j=v.size()-1;i<=j;i++,j--){
         int temp =v[i];
         v[i]=v[j];
-        v[j]=temp;
-        
+        v[j]=temp; 
     }
-    }
-     display(v);
+    display(v);
 }
