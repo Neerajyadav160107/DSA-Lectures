e312d252bef5f1cecffe75102ac00b352f3ab1e8
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-27 23:17:29 +0530

    Reversal Of array without another arraycd

--- Array2/tempCodeRunnerFile.cpp
+++ Array2/tempCodeRunnerFile.cpp
@@ -1,1 +1,9 @@
-v.at(2)=1;
+int i=0;
+    // int j=v.size()-1;
+    // while(i<=j){
+    //     //swap v[i] and v[j]
+    //     int temp =v[i];
+    //     v[i]=v[j];
+    //     v[j]=temp;
+    //     i++;
+    //     j--;
