b03785947439089a0f8d9b969eff66da9194ccc3
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-07-31 23:31:38 +0530

    Implement Dutch National Flag algorithm to sort 0s, 1s, and 2s in one pass.

---
+++ Array3/Sort0s1s2s.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std ;
+
+void sortnumbers(vector<int>&v){
+    int n=v.size();
+    int mid=0;
+    int low=0;
+    int high=n-1;
+    while (mid<=high){
+            if (v[mid]==2){
+                int temp=v[mid];
+                v[mid]=v[high];
+                v[high]=temp;
+                high--;
+            }
+            else if(v[mid]==0){
+                int temp=v[mid];
+                v[mid]=v[low];
+                v[low]=temp;
+                low++;
+                mid++;
+            }
+            else {
+                mid++;
+            }
+        
+    }
+    return;
+}
+
+int main () {
+    vector <int> v;
+    v.push_back(2);
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(1);
+    for ( int i=0; i<v.size();i++){
+        cout<<v[i]<< " ";
+    }
+    cout<<endl;
+
+    sortnumbers(v);
+    for ( int i=0; i<v.size();i++){
+        cout<<v[i]<< " ";
+    }
+    cout<<endl;  
+}
+// Logic:
+// Maintain three pointers: low, mid, and high.
+// Place 0s at the beginning, 2s at the end, and leave 1s in the middle.
