1ad6857d7c20b450051b48974b3d12d13cec3767
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-08-02 23:54:56 +0530

    Implement merge of two sorted arrays using two pointers

--- Array3/Mergetwosortedarray.cpp
+++ Array3/Mergetwosortedarray.cpp
@@ -6,35 +6,53 @@ vector<int> merge(vector<int>& arr1, vector<int>& arr2) {
     int n = arr1.size();
     int m = arr2.size();
     vector<int> res(n + m);
-    int i = 0;
-    int j = 0;
-    int k = 0;
-
-    while (i < n && j < m) {
-        if (arr1[i] < arr2[j]) {
-            res[k++] = arr1[i++];
-        } else {
-            res[k++] = arr2[j++];
+    int i = 0; // arr1
+    int j = 0; // arr2
+    int k = 0; // res
+    while(i<=n-1 && j<=m-1){
+        if(arr1[i]<arr2[j]){
+            res[k] = arr1[i];
+            i++;
+            k++;
+        }
+        else{ // arr[j]< arr[i] 
+            res[k]=arr2[j];
+            j++;
+            k++;
+        }
+        // for remaining elements of arr2 
+        if (i==n){ // arr1 all elements has been traversed
+            while (j<m){
+                res[k]=arr2[j];
+                k++;
+                j++;
+            }
+        }
+        if (j==m){ // arr2 all elements has been traversed }
+            while (i<=n-1){
+                res[k]=arr1[i];
+                k++;
+                i++;
+            } 
+    
         }
     }
-
-    while (i < n) {
-        res[k++] = arr1[i++];
-    }
-
-    while (j < m) {
-        res[k++] = arr2[j++];
-    }
-
-    return res;
-}
+    return res;  
+}  
 
 int main() {
     vector<int> arr1 = {1, 4, 5, 8};
     vector<int> arr2 = {2, 3, 6, 7, 10, 12};
+    for (int i = 0; i < arr1.size(); i++) {
+        cout << arr1[i] <<" ";
+    }
+    cout <<"";
+    for (int i = 0; i < arr2.size(); i++) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
 
     vector<int> v = merge(arr1, arr2);
-
     for (int i = 0; i < v.size(); i++) {
         cout << v[i] << " ";
     }
@@ -42,3 +60,7 @@ int main() {
 
     return 0;
 }
+// Logic:
+// Compare the current elements of both sorted arrays.
+// Insert the smaller element into the result array and move its pointer forward.
+// After one array is exhausted, copy the remaining elements of the other array.
