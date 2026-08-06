75c85b3651f9db1c17e1649ad449da8a70e69b50
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-08-06 21:28:33 +0530

    Introduce 2D arrays: declaration, syntax, and basic concepts

---
+++ 2dArray/Intro.cpp
@@ -0,0 +1,14 @@
+// 2D Array:
+// A 2D array is an array of arrays that stores elements in rows and columns.
+// It is used to represent tabular or matrix-like data.
+// A 2D array is a collection of elements of the same data type arranged in rows and columns.
+
+// syntax -> cout<<brr[row number][column number]
+// Declaration:
+// A 2D array is declared by specifying its data type, name,
+// number of rows, and number of columns.
+
+int arr[3][4];   // 3 rows, 4 columns
+// format 
+// data_type array_name[rows][columns];
+
