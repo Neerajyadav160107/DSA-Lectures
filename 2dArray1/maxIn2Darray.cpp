0a65145a5b4c40e1b89698165b2e01e093391ff6
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-08-09 23:03:58 +0530

    Maximum element in 2d array

---
+++ 2dArray1/maxIn2Darray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main () {
+    int m ;
+    cout<<"Enter the no of rows: "<<endl;
+    cin>>m;
+    int n;
+    cout<<"Enter the no of columns: "<<endl;
+    cin>>n;
+    int arr[m][n];
+    for (int i=0;i<=m-1;i++){
+        for (int j=0;j<=n-1;j++){
+            cin>>arr[i][j];
+        } 
+    }
+    // max
+    int max = INT_MIN;
+    for (int i=0;i<=m-1;i++){
+        for (int j=0;j<=n-1;j++){
+            if (max < arr[i][j])
+                max = arr[i][j];
+        } 
+    }
+    cout<<max;
+}    
