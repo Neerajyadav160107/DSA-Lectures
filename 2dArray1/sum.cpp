cdf9287b79a8cb56e44306e924adee86a6e30d17
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-08-10 16:04:40 +0530

    Sum of a 2d array

---
+++ 2dArray1/sum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main () {
+    int m ;
+    cout<<"Enter the no of rows: "<<endl;
+    cin>>m;
+    int n;
+    cout<<"Enter the no of columns: "<<endl;
+    cin>>n;
+    int arr[m][n];
+    for (int i=0;i<=m-1;i++){
+        for (int j=0;j<=n-1;j++){
+            cin>>arr[i][j];
+        } 
+    }
+    //sum
+    int sum =0 ;
+    for (int i=0;i<=m-1;i++){
+        for (int j=0;j<=n-1;j++){
+            sum += arr[i][j];
+        } 
+    }
+    cout<<sum;
+}   
