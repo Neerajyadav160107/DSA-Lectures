cf48fb41580c276f764b08eee8a8ab4b222444fb
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-08-17 23:04:57 +0530

    Transpose matrix in-place using swapping

--- 2dArray1/tempCodeRunnerFile.cpp
+++ 2dArray1/tempCodeRunnerFile.cpp
@@ -1,5 +1,40 @@
- // for (int i=0; i<2;i++){
-    //     for (int j=0;j<3;j++){
-    //         res[i][j] = a[i][j]+b[i][j];
-    //     }
-    // }
+#include<iostream>
+using namespace std;
+int main () {
+    int m;
+    cout<<"Enter no of rows/columns : ";
+    cin>>m;
+    int arr[m][m];
+    for ( int i=0 ; i<=m-1 ; i++){
+        for (int j =0; j<=m-1; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+    for ( int i=0 ; i<=m-1 ; i++){
+        for (int j =0; j<=m-1; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+    // transpose the same matrix 
+    for ( int i=0 ; i<m-1; i++){
+        for (int j=i+1; j<=m-1; j++){
+            //swappping of i,j and j,i
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+    }
+    cout <<endl;
+    // print 
+    for ( int i=0 ; i<=m-1 ; i++){
+        for (int j =0; j<=m-1; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}    
