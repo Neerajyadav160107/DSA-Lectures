a00db3b05357750064491344660956dda3387d56
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-08-26 23:57:53 +0530

    Wave form matrix

---
+++ 2dArray2/waveform.cpp
@@ -0,0 +1,32 @@
+//if row = (2n+1) row in --->
+// if row=2n then row is <---
+#include <iostream>
+using namespace std;
+int main () {
+    int m;
+    cout<<"enter row of matrix: ";
+    cin >> m;
+    int n;
+    cout <<"enter column of matrix : ";
+    cin >> n;
+    int arr[m][n];
+    for ( int i=0 ; i<m ; i++){
+        for (int j=0 ; j<n ; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+    // wave print ;
+    for ( int i=0 ; i<m ; i++){
+        if (i%2 == 0) {
+            for (int j=0; j<n ;j++){
+            cout<<arr[i][j]<<" ";
+            }
+        }    
+        else { // i = 1 ,3 , 5 
+            for ( int j =n-1 ; j>=0 ; j--){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+    }
+}                    
