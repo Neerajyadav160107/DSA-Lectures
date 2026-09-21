59ab20ede064c19692512751d419089f7b239f23
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-09-21 14:23:11 +0530

    String as a Data Type

--- strings/stringAsaDataType.cpp
+++ strings/stringAsaDataType.cpp
@@ -2,12 +2,13 @@
 #include<string>
 using namespace std;
 int main (){
-    // string str="SURAJ"; // character array size of 6 
+    // string str="SURAJ is at Amazon Office"; // character array size of 6 
     // cout<<str;
-    // string s;
+    string s;
     // cin>>s;
     // cout<<s;
-    string s;
-    cin>>s;
+    // string s;
+    // cin>>s; // only if the given string has no space , happens at input basically 
+    getline(cin,s); // instead of cin 
     cout<<s;
 }   
