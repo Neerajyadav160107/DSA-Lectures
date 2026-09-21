1f12081222f14e4a688f68304422334fe1b4d898
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-09-21 14:50:26 +0530

    Count no of vowels

---
+++ strings/ifvowel.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+    string str = "Suraj Parmar";
+    int count = 0;
+    int i = 0;
+    while (str[i]!='\0'){
+        if(str[i]=='a' || str[i]=='e'|| str[i]=='i' || str[i]=='o' || str[i]=='u')
+            count++;
+        i++;
+    }
+    cout<<count;
+}
