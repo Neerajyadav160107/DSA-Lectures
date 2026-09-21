90f65cd49bae7a8239a6683500734023b4c06dd5
Neeraj Yadav <neerajyadav160107@gmail.com>
2026-09-21 14:11:15 +0530

    Strings Intro

---
+++ strings/introstrings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main () {
+    char str[] = {'A', 'B', 'C', 'D', 'E'};
+    // for ( int i=0;str[i]!='\0';i++){
+    //     cout<<str[i]<<" ";
+    // }
+    cout<<str;
+    // // char str[5] ="abcd";
+    // for (int i=0; i<5;i++){
+    //     cout<<str[i]<<" ";
+    // }
+    // cout<<(int)(str[1]);
+}
---
+++ strings/stringAsaDataType.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main (){
+    // string str="SURAJ"; // character array size of 6 
+    // cout<<str;
+    // string s;
+    // cin>>s;
+    // cout<<s;
+    string s;
+    cin>>s;
+    cout<<s;
+}   
